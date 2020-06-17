af6c95dc0731ba7780e9f21d2ae1edbdce7dd672
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-06-17 14:26:55 +0200

    Tidying

--- src/services/world-time-api.service.ts
+++ src/services/world-time-api.service.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +8,11 @@ import { HttpClient } from '@angular/common/http';
 export class WorldTimeApi {
   url = 'http://worldtimeapi.org/api/timezone/'
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   // Get JSON with time and timezone info from url
   getTimezoneInfo(timezone: string): Observable<any> {
     return this.http.get(this.url + timezone)
   }
+
 }
